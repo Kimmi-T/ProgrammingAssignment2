74512513b19883db00e6310c46badffb37dcdbdc
Kimmi <73082357+Kimmi-T@users.noreply.github.com>
2020-11-01 15:52:12 -1000

    Programming Assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheMatrix combination will calculate the
+## inverse of the inputted matrix, cache it's inverse, and retrieve the
+## inverse if it exists in the cache.
+## The functions levy lexical scoping in order to retrieve variables
+## from the parent function that are left unspecified.
 
-## Write a short comment describing this function
+## The set function specifies which data points to access.
+## Variable x is assigned to the input argument in the parent function.
+## Variable m is defaulted to NULL in the parent function in order to
+## clear the cache.
+## The get function is solving for the matrix inverse.
+## This get function is looking to the parent environment to obtain the 
+## correct definition of variable m 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
+## The cachesolve function will retrieve data from the cache if it exists
+## for the function argument, otherwise, it will calculate the matrix
+## inverse
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
+
